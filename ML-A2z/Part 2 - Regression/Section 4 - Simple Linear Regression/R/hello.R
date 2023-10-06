fd7b5530fbe88c12b626c2d117e7a4486180dcc1
Mohammed Ziedan <don.nabil@gmail.com>
2023-10-06 20:43:34 +0000

    adding ML-A2Z materialss

---
+++ ML-A2z/Part 2 - Regression/Section 4 - Simple Linear Regression/R/hello.R
@@ -0,0 +1,6 @@
+# setwd('/workspaces/codespaces-jupyter/ML-A2z/Part 2 - Regression/Section 4 - Simple Linear Regression/R')
+# setwd('./')
+
+library(readr)
+
+data <- read.csv("Salary_Data.csv", header = TRUE)
